185b6bcf86421d99c2e64362592a17a51e46335b
Xlaugts <henriquepirani@gmail.com>
2018-07-13 11:19:14 +0100

    VR tutorials project

---
+++ Assets/MyData/Scripts/Collectable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+    Vector3 spin;
+
+	// Update is called once per frame
+	void Update () {
+        spin = new Vector3(5f, 5f, 0f);
+
+        transform.Rotate(spin);
+	}
+}
---
+++ Assets/MyData/Scripts/PlayerWalk.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour {
+
+    public float playerSpeed;
+    public GameObject wall;
+    public int count;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        if (Input.GetButton("Fire1"))
+        {
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        }
+        if(count == 4)
+        {
+            wall.SetActive(false);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+            count++;
+        }
+    }
+}
